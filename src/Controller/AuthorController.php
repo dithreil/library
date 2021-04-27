35d45c2695671724c2f909547d2c354f4af086c8
Вячеслав Сурин <dithreil@hotmail.com>
2021-04-27 14:18:29 +0300

    Добавлен список авторов на главной странице, кнопка в нав бар для добавления случайного автора

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -4,12 +4,16 @@
 namespace App\Controller;
 
 
+use App\Entity\Author;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class AuthorController
+class AuthorController extends AbstractController
 {
     public function index()
     {
         return new Response('Author Controller');
     }
+
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 
+use App\Entity\Author;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,13 +13,41 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="main_page")
      */
     public function index()
     {
-        $authors = ['Author1', 'Author2'];
+        $authors = $this->getDoctrine()->getRepository(Author::class)->findAll();
+
         // return new Response('Home Controller');
         return $this->render('home/index.html.twig', array('authors' => $authors));
+    }
+
+    /**
+     * Добавить случайного автора
+     * @Route("/author", name="create_author")
+     */
+    public function createAuthor(): Response
+    {
+        $names = ['Алан','Ирвинг','Джордж','Кори','Эрнест','Терри','Нил','Роджер','Теодор','Стивен'];
+        $surnames = ['Брэдли', 'Стоун', 'Мартин', 'Тейлор', 'Хемингуэй',
+            'Прачетт', 'Стивенсон', 'Желязны', 'Драйзер', 'Кинг'];
+
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createAuthor(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $author = new Author();
+        $author->setName($names[random_int(1,10)]);
+        $author->setSurname($surnames[random_int(1,10)]);
+
+        // tell Doctrine you want to (eventually) save the Author (no queries yet)
+        $entityManager->persist($author);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->redirectToRoute('index');
     }
 
 }
